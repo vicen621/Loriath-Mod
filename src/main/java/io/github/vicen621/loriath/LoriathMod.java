58fe844630e9c7f90711a10550ea2aa01d5da653
vicen621 <71749981+vicen621@users.noreply.github.com>
2022-12-29 00:09:30 -0300

    refactor: added another todo

--- src/main/java/io/github/vicen621/loriath/LoriathMod.java
+++ src/main/java/io/github/vicen621/loriath/LoriathMod.java
@@ -12,7 +12,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 //TODO Arreglar Infinite torch que rompe los bloques transparentes
-//TODO https://discord.com/channels/@me/1045529136657072138/1057856591199748196 
+// https://discord.com/channels/@me/1045529136657072138/1057856591199748196 
+
+//TODO Agregar los hats que faltan
+// https://cdn.discordapp.com/attachments/1045529136657072138/1057387840427806720/hats_que_faltan.rar
 public class LoriathMod implements ModInitializer {
     public static final String MODID = "loriath";
     public static final Logger LOGGER = LoggerFactory.getLogger("Loriath");
